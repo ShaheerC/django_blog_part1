9f3e14936138b788a96822ca21237d23f3acfe46
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-08-06 16:51:43 -0400

    various unsuccessful attempts  to add validation for draft and pub_date

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.forms import ModelForm
 from django.core.validators import URLValidator, MinLengthValidator, MaxLengthValidator
 from django.core.exceptions import ValidationError
-from datetime import datetime
+from datetime import datetime, date
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
@@ -14,11 +14,44 @@ class Article(models.Model):
     def __str__(self):
         return (f"{self.title} by {self.author}")
 
+    def clean(self):
+         if self.draft and self.published_date < date.today():
+           raise ValidationError('If this is a draft, the publish date must be in the future.')
+    #     if self.draft and self.published_date < date.today():
+            # trigger model validation error
+
+
 class ArticleForm(ModelForm):
 
     class Meta:
         model = Article
         fields = ["title", "body", "author", "draft", "published_date"]
+    
+    # if the article has draft set to True, 
+    #     the published_date must be in the future. 
+    # If draft is set to False, 
+    #     published_date must be in the past.
+
+    # def clean(self): 
+  
+    #     # data from the form is fetched using super function 
+    #     super(ArticleForm, self).clean() 
+          
+    #     published_date = self.cleaned_data.get('published_date') 
+    #     draft = self.cleaned_data.get('draft') 
+  
+    #     # conditions to be met for the username length 
+    #     if (draft == True) and (published_date.date() > date.today()):
+    #         self._errors['published_date'] = self.error_class([ 
+    #             'Published Date must be in the future']) 
+        
+    #     if (draft == False) and (published_date.date() < date.today()):
+    #         self._errors['published_date'] = self.error_class([ 
+    #             'Published Date must be in the future']) 
+  
+    #     return self.cleaned_data 
+           
+
 
 class Comment(models.Model):
     name = models.CharField(max_length=255)
